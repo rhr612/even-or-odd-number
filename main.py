01bfbd6b2b79e97ea5b9cb0a4b41eacbaaa90777
RHR <rakibhassanrakib612@gmail.com>
2023-01-16 01:01:02 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+n=int(input('Input Number\n'))
+
+rem=n%2
+
+if n==0:
+    print('This is a ZERO')
+
+else:
+    if rem==0:
+        print('The number is EVEN')
+    else:
+        print('The number is ODD')
